422aac4e3b5dfca94a30ce1aa2582ef245823343
maryammuhammadafzal <natiqnagori7@gmail.com>
2025-06-08 14:22:03 +0500

    Creating New Sections

--- app/Portfolio/Page.jsx
+++ app/Portfolio/Page.jsx
@@ -1767,11 +1767,13 @@ import {
   ChevronDown,
   ChevronLeft,
   ChevronRight,
+  Icon,
 } from "lucide-react";
 import Image from "next/image";
 import Head from "next/head";
 import { TwitterLogoIcon } from "@radix-ui/react-icons";
 import GitHubContributions from "@/components/GitHubContributions";
+import Link from "next/link";
 
 const navItems = [
   { name: "Home", href: "#home" },
@@ -1810,6 +1812,24 @@ const projects = [
     image2: "/mockup-6.webp",
   },
 ];
+
+const contacts = [
+  {
+    url : "",
+    text: "abc",
+    service: "email"
+  },
+  {
+    url : "",
+    text: "abc",
+    service: "email"
+  },
+  {
+    url : "",
+    text: "abc",
+    service: "email"
+  }
+]
 
 const Portfolio = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -2073,18 +2093,47 @@ const Portfolio = () => {
         </div>
       </section>
 
-      <section className="flex bg-neutral-100 w-full px-3 py-20 justify-center h-auto">
-        <div className="w-[93%] flex flex-col gap-20">
-          <h3 className="text-[120px] uppercase font-bold">Let's Connect</h3>
-          <div className="w-full h-auto flex gap-20">
-            <div className="w-[50%] p-3 h-auto flex justify-center items-center">
-              <div className="clock rounded-lg w-[600px] h-[300px] bg-white p-3 flex justify-center items-center">
+      <section aria-label="Clock and contacts section" id="contacts" className="container flex flex-col gap-10">
+      <motion.h2 >Let’s connect</motion.h2>
 
-              </div>
-            </div>
-          </div>
+      <motion.div className=" flex flex-col-reverse lg:flex-row justify-between items-start gap-10">
+        <div className="flex justify-start items-center w-full xl:w-1/2">
+          {/* <DigitalClockDynamic /> */}
         </div>
-      </section>
+        <div className=" flex flex-col justify-between items-center gap-10 w-full xl:w-1/2 ">
+          <p>
+            Thank you for stopping by! I’m always excited to explore new
+            opportunities and collaborations. Let’s connect through email,
+            phone, LinkedIn, or GitHub—I’d love to hear from you!
+          </p>
+          <ul className="flex flex-col md:flex-row flex-wrap justify-start lg:justify-between items-start gap-4 lg:gap-2 w-full">
+            {contacts.map((contact, index) => {
+              return (
+                <li key={index} className="flex flex-col items-start gap-1">
+                  <Link
+                    href={contact.url}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    aria-label={`Link to contact ${contact.url}`}
+                    className="group flex flex-col items-start font-bold gap-2 cursor-pointer transition-all duration-300 hover:text-grey_500"
+                  >
+                    {contact.text}
+                    <Icon
+                      id="icon-arrow-up-right"
+                      width={12}
+                      height={12}
+                      className="text-black group-hover:text-grey_500"
+                    />
+                  </Link>
+                  <p className="">{contact.service}</p>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </motion.div>
+    </section>
+
     </div>
   );
 };
--- app/layout.js
+++ app/layout.js
@@ -12,14 +12,38 @@ const suse = SUSE({
   weight: [ '400']
 });
 
+// export const metadata = {
+//   title: "Portfolio | Mern Stack Developer",
+//   description: "Showcasing my skills and projects as a mern stack developer",
+// };
+
 export const metadata = {
-  title: "Portfolio | Mern Stack Developer",
-  description: "Showcasing my skills and projects as a mern stack developer",
+  title: "Full Stack Dev Maryam Afzal",
+  description:
+    "Hi, I'm a full-stack developer with an engineering background, specializing in scalable and efficient solutions for both team and individual projects.",
+  openGraph: {
+    title: "Full Stack Dev Maryam Afzal",
+    description:
+      "Hi, I'm a full-stack developer with an engineering background, specializing in scalable and efficient solutions for both team and individual projects.",
+    url: "https://my-portfolio-website-two-psi.vercel.app/", 
+    type: "website",
+    images: [
+      {
+        url: "images/full_stack_dev_maryam_afzal.png", // Full URL required
+        width: 1200,
+        height: 630,
+        alt: "Full Stack Dev Maryam Afzal",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" type="image/svg+xml" href="/icons/favicon.svg"></link>
+      </head>
       <body className={`${monaSans.variable}`}>
         {children}
       </body>
